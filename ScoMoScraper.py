2f647cb8f2ffd767bd51c74e4b93deb4ea30fe3c
Timothy Gregg <hello@timgregg.net>
2018-10-25 17:47:45 +1100

    Add some helpful comments

--- ScoMoScraper.py
+++ ScoMoScraper.py
@@ -1,21 +1,22 @@
-import sqlite3
-import requests
-import re
-import time
+import sqlite3 # for saving data in an SQLite database
+import requests # for making http requests to the PM's website
+import time # for converting dates and times to a standardised format
 from time import mktime
 from datetime import datetime
-from bs4 import BeautifulSoup
-    
+from bs4 import BeautifulSoup # for parsing HTML and extracting data
 
+
+# the following functions extract important data from a new post on the PM's website...
+
+# extract the title 
 def get_title(media_item):
-    # iterate through child elements of media item to get title
     children = media_item.descendants
     for child in children:
         if child.name == 'div' and child.get('class', '') == ['media-title']:
             latest_title = child.text
             return latest_title
 
-
+# extract the URL   
 def get_url(media_item):
     children = media_item.descendants
     for child in children:
@@ -24,20 +25,18 @@ def get_url(media_item):
                 latest_url = a.get('href') 
                 return latest_url
 
-
-
+# extract the date, convert it to datetime   
 def get_date(media_item):
-    # iterate through child elements of media item to get date
     children = media_item.descendants
     for child in children:
         if child.name == 'div' and child.get('class', '') == ['media-date']:
             latest_date = child.text
-            # convert date to datetime 
             format = '%d %b %Y'
             convert = time.strptime(latest_date, format)
             latest_datetime = int(mktime(convert))
             return latest_datetime
 
+# check the database to see if latest article is already saved; if not, save it
 def check_db(title, datetime, content):
     c.execute('SELECT * FROM articles ORDER BY datetime DESC LIMIT 1')
     record = c.fetchone()
@@ -52,15 +51,13 @@ def check_db(title, datetime, content):
         print('table updated: ' + latest_title + ' @ ' + str(latest_datetime))
         return True        
 
-
+# open the page of the latest artice, parse its contents
 def read_page(href):
     url = 'https://pm.gov.au' + href
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, "lxml")
     article = soup.find('article')
     return article.text
-
-
 
 
 # TEMP ONLY: timestamp for database files
@@ -70,12 +67,13 @@ timeNow = time.time()
 conn = sqlite3.connect('ScoMoScraper ' + str(timeNow) + '.db')
 c = conn.cursor()
 
+# ////////WORK IN PROGRESS///////////////
 # TO DO: conditional logic, create table if no table else pass .......... if the table doesn't exist, Python will throw an exception and a new table will be created 
-
 # try:
 #     c.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="articles"')
 # except sqlite3.OperationalError:
 #     c.execute('''CREATE TABLE articles (article_name text, datetime integer, article_content text)''')
+# /////////////////////////////////////
 
 c.execute('''CREATE TABLE articles (article_name text, datetime integer, article_content text)''')
 
